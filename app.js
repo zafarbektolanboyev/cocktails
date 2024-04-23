1f2e5b09b3d490b71a0f507c95a9e8f3c45e1af6
zafarbek tolanboyev <zafarvektolanboyev618@gmail.com>
2024-04-23 19:18:28 +0500

    appjsfileadd

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const coctailsCenter = document.querySelector(".coctails-center");
+const cocktailsCenter = document.querySelector(".cocktails-center");
 const loader  = document.querySelector(".loader");
 
 fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
@@ -10,12 +10,12 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
 
 function creatDrinks(drinks){
     drinks.forEach(
-        ({idDrink, strDrink, strGlass, strAlcoholic, strDrinkThumb}) =>{
+        ({idDrink, strDrink, strGlass, strAlcoholic, strDrinkThumb}) => {
         const article = document.createElement("article");
 
-        article.classList.add("coctail")
+        article.classList.add("cocktail");
 
         article.innerHTML = `<div class="img-container"><img src=${strDrinkThumb} alt=${strDrink}></div><div class="cocktail-footer"><h3>${strDrink}</h3><h4>${strGlass}</h4><p>${strAlcoholic}</p><a class="btn btn-primary btn-details" href="./about.html?id=${idDrink}">details</a></div>`;
-        coctailsCenter.appendChild(article);
+        cocktailsCenter.appendChild(article);
     });
 }
